a815bc6bc5009ec5b814badbf4bc05116fbfab17
Livia130503 <mt221054@fhstp.ac.at>
2025-03-11 12:06:43 +0100

    DataLayer Erstversion

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Quiz } from './Quiz';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  public currentQuiz: Quiz = {id: '', quizName: 'newQuiz', questions: []};
 
-  constructor() { }
+  constructor() {
+    this.currentQuiz.questions.push({
+      id: '1',
+      title: 'What is the capital of France?',
+      a1: 'Paris',
+      a2: 'London',
+      a3: 'Berlin',
+      a4: 'Madrid',
+      correct: 1
+      })
+   }
 }
