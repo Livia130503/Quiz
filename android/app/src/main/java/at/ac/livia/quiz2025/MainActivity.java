cba8222ec78776a666455053a27c70f35f9b5de9
Livia130503 <mt221054@fhstp.ac.at>
2025-03-18 13:38:25 +0100

    Andriod und ISO build

---
+++ android/app/src/main/java/at/ac/livia/quiz2025/MainActivity.java
@@ -0,0 +1,5 @@
+package at.ac.livia.quiz2025;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'at.ac.livia.quiz2025',
   appName: 'Quiz',
   webDir: 'www'
 };
